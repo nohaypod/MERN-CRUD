7fc65b34d5e88bfd085f22347704a97ae9252316
Aldo Ortega González <aeledeo.9@gmail.com>
2024-06-30 13:08:47 -0600

    version 1.0.1 conectando a BD en db.js para test de producción

--- src/db.js
+++ src/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB= async () => {
     try{
-        await mongoose.connect("mongodb://localhost/merndb");
+        //await mongoose.connect("mongodb://localhost/merndb");
+        await mongoose.connect ("mongodb+srv://<username>:<password>@clus.nrgvdcg.mongodb.net/?retryWrites=true&w=majority&appName=Clus");
         console.log(">>> DB logró conectarse aqui");
     } catch(error){
         console.log("error");
